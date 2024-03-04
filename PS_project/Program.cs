9a18337493351ae2876267ba913b096537ba3f27
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2024-03-04 23:55:27 +0200

    initial commit

---
+++ PS_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PS_project
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
